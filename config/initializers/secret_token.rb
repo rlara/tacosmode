bb15a11f75ad1e3cc67f8ee0dbc5dc9f47fb687a
rlara <rlara@incaztech.com>
2012-07-25 11:38:45 -0500

    firs commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tacosmode::Application.config.secret_token = '5b62adddf6a17d91eb81b8ce3e25136c3abae45c67f8db48819fe77964ebfd1e5112b45ae8729fdde35c756f2019d135cd31ca1e24f29fa6e4b6ce1295bf5fa6'
