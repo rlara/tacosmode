12a80e6b4201fb9650bc8f6336463d7062981f98
rlara <rlara@incaztech.com>
2012-08-12 11:47:55 -0500

    jquery

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   has_many :stock_items
   has_many :transfered_products
-  attr_accessible :date, :lote, :name, :sku, :units
+  attr_accessible :date, :lote, :name, :sku, :units, :price
 
   validates :name, :lote, :sku, :units, :presence => true
 
